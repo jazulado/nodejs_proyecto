f313ef882635e014afa45963dd294697adebc9d6
505 <505@academicos.cesde.edu.co>
2020-02-18 08:04:48 -0500

    Inicio

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api', function (req, res) {
+  let nombre = "julian"
+  let apellido = "usuga"
+  res.send(`El nombre es: ${nombre} y apellido ${apellido}`);
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+//git push -u origin master
