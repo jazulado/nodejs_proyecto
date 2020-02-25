be0dfc9a5e8d9bae93d204c341f233038be7aa7c
505 <jazulado1337@hotmail.com>
2020-02-25 08:25:46 -0500

    mongoose

--- dist/index.js
+++ dist/index.js
@@ -4,19 +4,35 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express2.default)(); //var express = require('express');
+//var express = require('express');
+var app = (0, _express2.default)();
 
-
+_mongoose2.default.Promise = global.Promise;
+_mongoose2.default.connect("mongodb://localhost:27017/cursos", { useNewUrlParser: true, useUnifiedTopology: true }).then(function () {
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+  app.get('/', function (req, res) {
+    var nombre = "julian";
+    var apellido = "usuga";
+    res.send('El nombre es: ' + nombre + ' y apellido ' + apellido);
+  });
+}).catch(function (error) {
+  app.listen(3000, function () {
+    console.log('App error ' + error);
+  });
+});
 app.get('/api', function (req, res) {
   var nombre = "julian";
   var apellido = "usuga";
   res.send('El nombre es: ' + nombre + ' y apellido ' + apellido);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 //git push -u origin master
 //cmdkey /delete:LegacyGeneric:target=git:https://github.com
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 //var express = require('express');
 import express from 'express'
+import mongoose from 'mongoose'
 var app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/cursos",{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  app.listen(3000,() => {
+    console.log('Example app listening on port 3000!');
+  });
+  app.get('/', function (req, res) {
+    let nombre = "julian"
+    let apellido = "usuga"
+    res.send(`El nombre es: ${nombre} y apellido ${apellido}`);
+  });
+}).catch((error) => {
+  app.listen(3000,() => {
+    console.log('App error ' + error);
+});
 
+})
 app.get('/api', function (req, res) {
   let nombre = "julian"
   let apellido = "usuga"
   res.send(`El nombre es: ${nombre} y apellido ${apellido}`);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+
 //git push -u origin master
 //cmdkey /delete:LegacyGeneric:target=git:https://github.com
